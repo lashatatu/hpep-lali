3a01ac25462bd002a278e1c40f711d68569821af
lasha <lashatatu@gmail.com>
2023-01-08 02:58:37 +0400

    building banner component 2:36:09
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      not done
    directions                not done

--- app/(user)/administration/consultants/page.tsx
+++ app/(user)/administration/consultants/page.tsx
@@ -1,9 +1,78 @@
 const Page = () => {
   return (
-    <div>
-      <p>Page</p>
-    </div>
-  )
-}
+    <div className={"lt-container-little"}>
+      <h1 className={"lt-page-h1"}>ცენტრის კონსულტანტები</h1>
+      <div className={"lt-page-2grid lt-page-align-left"}>
+        <div className={"font-bold"}>არდიტ მენეი</div>
+        <div>აიოვას სახელმწიფო უნივერსიტეტის პროფესორი</div>
+        <div className={"font-bold"}>კარენ კაშმანიან ოატესი</div>
+        <div>
+          ჰარისბურგის მეცნიერებისა და ტექნოლოგიების უნივერსიტეტის პრორექტორი
+        </div>
+        <div className={"font-bold"}>მრ. ჯონ ჰალდენი</div>
+        <div>CCID-ის პრეზიდენტი/ აღმასრულებელი დირექტორი</div>
+        <div className={"font-bold"}>ვიოლეტ კალესნიკოვი</div>
+        <div>
+          CCID-ის კორპორაციული და საერთაშორისი განვითარების თანამეგობრობის
+          დეკანი
+        </div>
 
-export default Page
+        <div className={"font-bold"}>რამეშვარ ს. კანვარი</div>
+        <div>
+          პროფესორი და დეპარტამენტის მართველი აიოვას სახელმწიფო უნივერსიტეტში
+        </div>
+        <div className={"font-bold"}>ჯეფ არმსტრონგი</div>
+        <div>მუსკანტინის კოლეჯის დეკანი</div>
+        <div className={"font-bold"}>სტივენ კ. მაიკოლსონი</div>
+        <div>ასოცირებული პროფესორი</div>
+        <div className={"font-bold"}>სტივენ ა. ფრიმენი</div>
+        <div>
+          დირექტორი და ასისტენტ პროფესორი აიოვას სახელმწიფო უნივერსიტეტში
+        </div>
+        <div className={"font-bold"}>ემი ლეიჰ კალეიტა</div>
+        <div>ასოცირებული პროფესორი აიოვას სახელმწიფო უნივერსიტეტში</div>
+        <div className={"font-bold"}>კარლა ემბლეტონი</div>
+        <div>
+          საგანმანათლებლო ტექნოლოგიების დირექტორი აიოვას ტექნიკურ უნივერსიტეტში
+        </div>
+        <div className={"font-bold"}>მერი ლოუ ლაუერი</div>
+        <div>
+          დოქტორი და ვიცეპრეზიდენტი კირკველის კოლეჯში ემი შაჩთერი ასოცირებული
+          დეკანი ხელოვნებისა და მეცნიერებების კოლეჯში
+        </div>
+
+        <div className={"font-bold"}>დოქტორი დევით ლ თაუკი</div>
+        <div>da Vinci - ფაკულტეტის დირექტორი</div>
+        <div className={"font-bold"}>გიორგი ჭიღლაძე</div>
+        <div>კვლევების ასისტენტი აიოვას ტექნიკურ უნივერსიტეტში</div>
+        <div className={"font-bold"}>ლინდა არეშენკოფიროდნი ნოველი</div>
+        <div>ასოცირებული დეკანი ბიზნესის ინფორმაციის ტექნოლოგიებში WCTC</div>
+        <div className={"font-bold"}>გრაჰენ გეილი</div>
+        <div>კლიენტებთან მუშაობის მენეჯერი და თანამეგობრობის ტრენინგი WCTC</div>
+        <div className={"font-bold"}>მარკ იორგენსენი</div>
+        <div>ასოცირებული დეკანი მარკეტინგი, მენეჯმენტი და ხარისხი WCTC</div>
+        <div className={"font-bold"}>ვილიამ ბულოკი</div>
+        <div>ასოცირებული დეკანი სწავლება/მშენებლობის ტექნოლოგია I-156 WCTC</div>
+        <div className={"font-bold"}>ალედა ბოურასა</div>
+        <div>CGBP მენეჯერი გლობალური განათლება და სავაჭრო ცენტრი WCTC</div>
+        <div className={"font-bold"}>ჯერი ბოლტონი</div>
+        <div>
+          აგროკულტურების მეცნიერების დეკანი Washington Hallm, Kirkwood CC
+        </div>
+        <div className={"font-bold"}>დუგლას კანალი</div>
+        <div>
+          ასოცირებული დეკანი ჰარი ვ. ქუადრაცის ბეჭვდისა და გრაფიკული ცენტრში
+        </div>
+        <div className={"font-bold"}>კორი ვანეკი</div>
+        <div>ინსტრუქტორი CS/SS დეპარტამენტში</div>
+        <div className={"font-bold"}>დონ ჰოფსტრანდი</div>
+        <div>
+          აიოვას სახელმწიფო უნივერსიტეტი ბობ ელბი მუსკანტინის კოლეჯის
+          ასოცირებული დირექტორი
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
