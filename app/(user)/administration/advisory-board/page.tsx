4c597070c88e7f660883a84a9b0f8bdaa729faeb
lasha <lashatatu@gmail.com>
2023-01-08 01:53:02 +0400

    building banner component 2:36:09
    advisory-board            done
    advisory-committee        not done
    consultants               not done
    guests-of-the-center      not done
    directions                not done

--- app/(user)/administration/advisory-board/page.tsx
+++ app/(user)/administration/advisory-board/page.tsx
@@ -1,9 +1,46 @@
 const Page = () => {
   return (
-    <div>
-      <p>Page</p>
+    <div className={"lt-container"}>
+      <h1 className={"text-4xl text-center font-extrabold pt-8 pb-10"}>
+        მრჩეველთა საბჭო
+      </h1>
+      <h2 className={"pl-2 font-bold"}>მრჩეველთა საბჭო</h2>
+      <div>
+        <ul className={'pl-10 pt-4 pb-10'}>
+          <li className={"list-disc"}>
+            ფრანგიშვილი არჩილ, საქართველოს ტექნიკური უნივერსიტეტის რექტორი
+          </li>
+          <li className={"list-disc"}>
+            მოწონლიძე არჩილ, საქართველოს ტექნიკური უნივერსიტეტის ყოფილი რექტორი
+          </li>
+          <li className={"list-disc"}>
+            კლიმიაშვილი ლევან, საქართველოს ტექნიკური უნივერსიტეტის ვიცე რექტორი
+          </li>
+          <li className={"list-disc"}>
+            ზუმბურიძე ოთარ, საქართველოს ტექნიკური უნივერსიტეტის საერთაშორისო
+            ურთიერთობების დეპარტამენტის და სტანდარტების ხელმძღვანელი
+          </li>
+        </ul>
+      </div>
+      <h2 className={"pl-2 font-bold"}>სტუს ფაკულტეტების დეკანები</h2>
+      <div>
+        <ul className={'pl-10 pt-4 pb-10'}>
+          <li className={"list-disc"}>სამოქალაქო მშენებლობის ფაკულტეტი,</li>
+          <li className={"list-disc"}>ენერგეტიკისა და კომუნიკაციის ფაკულტეტი,</li>
+          <li className={"list-disc"}>სამთო-გეოლოგიური ფაკულტეტი,</li>
+          <li className={"list-disc"}>ქიმიური ტექნოლოგიისა და მეტალურგიის ფაკულტეტი,</li>
+          <li className={"list-disc"}>სატრანსპორტო და მანქანათმშენებლობის ფაკულტეტი,</li>
+          <li className={"list-disc"}>არქიტექტურის, ურბანისტიკის და დიზაინის ფაკულტეტი ,</li>
+          <li className={"list-disc"}>ჰუმანიტარული მიმართულებების ფაკულტეტი,</li>
+          <li className={"list-disc"}>ინფორმატიკისა და მართვის სისტემების ფაკულტეტი,</li>
+          <li className={"list-disc"}>
+            აგრარული მეცნიერებების და ბიოსისტემების ინჟნერინგის ფაკულტეტი,
+          </li>
+        </ul>
+      </div>
+      <p>ქართული ბიზნესისა და ინდუსტრიის წარმომადგენლები</p>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
