32d8b743d8a5b51d9b8cf927ee9b00120dc6cc31
lasha <lashatatu@gmail.com>
2023-01-19 18:27:38 +0400

    building banner component 2:36:09
    fixed aligment
    
    Administration            done
    
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      done
    directions                done
    
    Workshops                 done
    
    trainings                 done
    seminars                  done
    us-summer school          done
    
    Projects                  done
    
    current-projects          done
    finished-projects         done
    
    Library
    
    new-textbooks             done
    supporting-materials      partially done needs review
    center-library            not done
    gallery                   not done
    memorandums               not done

--- app/(user)/library/new-textbooks/page.tsx
+++ app/(user)/library/new-textbooks/page.tsx
@@ -1,9 +1,159 @@
 const Page = () => {
   return (
-    <div>
-      <p>Page</p>
-    </div>
-  )
-}
+    <div className={"lt-container-small"}>
+      <h1 className={"lt-page-h1"}>ახალი სახელმძღვანელოები</h1>
+      <div className={"mx-10"}>
+        <h2 className={"lt-page-h2 text-center"}>
+          საგნები უმაღლესი პროფესიული განათლებისთვის ბიზნეს ადმინისტრირების
+          სპეციალობაში
+        </h2>
+        <h3 className={"lt-page-h3"}>
+          სტუ-ს, გორის, წნორის და ახალქალაის უნივერსიტეტებისათვის
+        </h3>
+        <div className={"lt-page-2grid"}>
+          <div>
+            <h4 className={"lt-page-h4"}>I კურსის სახელმძღვანელოები</h4>
+            <p className={'indent-0'}>ბიზნეს მათემატიკა</p>
 
-export default Page
+            <p className={'indent-0'}>გარემო და მდგრადი განვითარება</p>
+
+            <p className={'indent-0'}>მცირე ბიზნესი</p>
+
+            <p className={'indent-0'}>კარიერა</p>
+
+            <p className={'indent-0'}>თანამედროვე ბიზნესი</p>
+
+            <p className={'indent-0'}>Interpersonal communications</p>
+
+            <p className={'indent-0'}>ბიზნეს და პროფესიული ინგლისური I</p>
+
+            <p className={'indent-0'}>ბიზნეს და პროფესიული ინგლისური II</p>
+
+            <p className={'indent-0'}>ეფექტური ბიზნეს მიმოწერა</p>
+
+            <p className={'indent-0'}>შესავალი კომპიუტრებეში</p>
+
+            <p className={'indent-0'}>კომპიუტერული აპლიკაციები</p>
+
+            <p className={'indent-0'}>ადმიანური ურთიერთობები</p>
+          </div>
+
+          <div>
+            <h4 className={"lt-page-h4"}>II კურსის სახელმძღვანელოები</h4>
+            <p className={'indent-0'}>ბიზნეს და პროფესიული ინგლისური III</p>
+
+            <p className={'indent-0'}>ბიზნეს და პროფესიული ინგლისური IV</p>
+
+            <p className={'indent-0'}>ადამიანური რესურსების მენეჯმენტი</p>
+
+            <p className={'indent-0'}>მშენებლობა I</p>
+
+            <p className={'indent-0'}>მშენებლობა II</p>
+
+            <p className={'indent-0'}>მშენებლობა III</p>
+
+            <p className={'indent-0'}>კომპიუტერული პროგრამები I</p>
+
+            <p className={'indent-0'}>MS Project, MS FrontPage</p>
+
+            <p className={'indent-0'}>კომპიუტერული პროგრამები II</p>
+
+            <p className={'indent-0'}>AutoCad</p>
+
+            <p className={'indent-0'}>ეკონომიკის საფუძვლები</p>
+
+            <p className={'indent-0'}>მშენებლობის ეკონომიკა</p>
+
+            <p className={'indent-0'}>ბიზნესის დაგეგმვა</p>
+
+            <p className={'indent-0'}>მათემატიკა ბიზნესისათვის II</p>
+
+            <p className={'indent-0'}>მოლაპარაკების წარმოება</p>
+
+            <p className={'indent-0'}>კლიენტთა მომსახურების პრინციპები</p>
+
+            <p className={'indent-0'}>მშენებლობის პროექტის ორგანიზაცია და მენეჯმენტი</p>
+
+            <p className={'indent-0'}>პროექტის მენეჯმენტი</p>
+
+            <p className={'indent-0'}>ბუღალტერიის საფუძვლები</p>
+
+            <p className={'indent-0'}>Modern Drafting</p>
+
+            <p className={'indent-0'}>სპეციალური ღონისძიების მოწყობა</p>
+
+            <p className={'indent-0'}>შესავალი ტურიზმში</p>
+
+            <p className={'indent-0'}>ზოგადი მენეჯმენტი</p>
+
+            <p className={'indent-0'}>მენეჯერის უნარები</p>
+
+            <p className={'indent-0'}>სამშენებლო მასალები</p>
+
+            <p className={'indent-0'}>კვლევები</p>
+
+            <p className={'indent-0'}>ლიდერობა</p>
+          </div>
+
+          <div>
+            <h4 className={"lt-page-h4"}>III კურსის სახელმძღვანელოები</h4>
+            <p className={"indent-0"}>ზოგადი ანერფრენერშიპი</p>
+
+            <p className={"indent-0"}>სასტუმროების მენეჯმენტი</p>
+
+            <p className={"indent-0"}>აუდიტის საფუძვლები</p>
+
+            <p className={"indent-0"}>ბიზნეს კანონები</p>
+
+            <p className={"indent-0"}>ელ-კომერცია</p>
+
+            <p className={"indent-0"}>გაყიდვები</p>
+
+            <p className={"indent-0"}>მარკეტინგის საფუძვლები</p>
+
+            <p className={"indent-0"}>მშენებლობის მენეჯმენტი</p>
+
+            <p className={"indent-0"}>ფინანსური მენეჯმენტი</p>
+
+            <p className={"indent-0"}>საინჟინრო მექანიკა</p>
+
+            <p className={"indent-0"}>რესტორნების მენეჯმენტი</p>
+
+            <p className={"indent-0"}>ბიზნეს მათემატიკა</p>
+
+            <p className={"indent-0"}>
+              მშენებლობის და უსაფრთხოების ზედამხედველობა
+            </p>
+
+            <p className={"indent-0"}>მცირე ბიზნესის ზედამხედველობა</p>
+
+            <p className={"indent-0"}>
+              სასტუმროების, რესტორნების და სპეციალური ღონისძიებბების
+              ზედამხედველობა
+            </p>
+
+            <p className={"indent-0"}>
+              Internship in Hotel and Restaurant Management Specialty
+            </p>
+
+            <p className={"indent-0"}>
+              Internship in Small Business Administration Specialty
+            </p>
+
+            <p className={"indent-0"}>Internship in Construction Specialty</p>
+
+            <p className={"indent-0"}>ბიზნეს ინგლისური</p>
+
+            <p className={"indent-0"}>სპეციალური ინგლისური I</p>
+
+            <p className={"indent-0"}>სპეციალური ინგლისური II</p>
+
+            <p className={"indent-0"}>სპეციალური ინგლისური III</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
