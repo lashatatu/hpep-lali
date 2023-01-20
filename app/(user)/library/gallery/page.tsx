1d80d89ef543aa1db1ef07f8eb7cb6b81bc99a95
lasha <lashatatu@gmail.com>
2023-01-20 17:30:09 +0400

    building banner component 2:36:09
    fixed aligment
    fixed menu bar focus
    
    Administration            done
    
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      done
    directions                done
    
    Workshops                 done
    
    trainings                 done
    seminars                  done
    us-summer school          done
    
    Projects                  done
    
    current-projects          done
    finished-projects         done
    
    Library
    
    new-textbooks             done
    supporting-materials      partially done needs review
    center-library            not done
    gallery                   done
    memorandums               not done

--- app/(user)/library/gallery/page.tsx
+++ app/(user)/library/gallery/page.tsx
@@ -1,9 +1,28 @@
+import { images } from "../../../../utilities/photos";
+import Image from "next/image";
+
 const Page = () => {
   return (
-    <div>
-      <p>Page</p>
+    <div className={"lt-container-small"}>
+      <h1 className={"lt-page-h1"}>გალერეა</h1>
+      <div className={"grid grid-cols-4 gap-y-8 gap-x-2"}>
+        {images.map((e, index) => (
+          <div className={"border-2 rounded-3xl"}>
+            <p className={"indent-0 text-primary text-center p-2"}>{e.text}</p>
+            <div>
+              <Image
+                key={index}
+                // @ts-ignore
+                src={e.image}
+                alt={e.text}
+                className={'mx-auto object-bottom'}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
---
+++ utilities/photos.ts
@@ -0,0 +1,218 @@
+import image1 from "../public/1.jpg";
+import image2 from "../public/2.jpg";
+import image3 from "../public/3.jpg";
+import image4 from "../public/4.jpg";
+import image5 from "../public/5.jpg";
+import image6 from "../public/6.jpg";
+import image7 from "../public/7.jpg";
+import image8 from "../public/8.jpg";
+import image9 from "../public/9.jpg";
+import image10 from "../public/10.jpg";
+import image11 from "../public/11.jpg";
+import image12 from "../public/12.jpg";
+import image13 from "../public/13.jpg";
+import image14 from "../public/14.jpg";
+import image15 from "../public/15.jpg";
+import image16 from "../public/16.jpg";
+import image17 from "../public/17.jpg";
+import image18 from "../public/18.jpg";
+import image19 from "../public/19.jpg";
+import image20 from "../public/20.jpg";
+import image21 from "../public/21.jpg";
+import image22 from "../public/22.jpg";
+import image23 from "../public/23.jpg";
+import image24 from "../public/24.jpg";
+import image25 from "../public/25.jpg";
+import image26 from "../public/26.jpg";
+import image27 from "../public/27.jpg";
+import image28 from "../public/28.jpg";
+import image29 from "../public/29.jpg";
+import image30 from "../public/30.jpg";
+import image31 from "../public/31.jpg";
+import image32 from "../public/32.jpg";
+import image33 from "../public/33.jpg";
+import image34 from "../public/34.jpg";
+import image35 from "../public/35.jpg";
+import image36 from "../public/36.jpg";
+import image37 from "../public/37.jpg";
+import image38 from "../public/38.jpg";
+import image39 from "../public/39.jpg";
+import image40 from "../public/40.jpg";
+import image41 from "../public/41.jpg";
+import image42 from "../public/42.jpg";
+import image43 from "../public/43.jpg";
+
+export const images= [
+  {
+    image: image1,
+    text: "ასუ და სტუ პარტნიორობის პროგრამა",
+  },
+  {
+    image: image2,
+    text: "ტრეინინგი ს&ს-ში აგრარული უნივერსიტეტის ლექტორთათვის",
+  },
+  {
+    image: image3,
+    text: "გორის მასწავლებლეთა პრეზენტაცია",
+  },
+  {
+    image: image4,
+    text: "გორის უნივერსიტეტის სტუდენტების აქტივობები",
+  },
+  {
+    image: image5,
+    text: "გორის სკ-ს პრაქტიკული სამუშაოები",
+  },
+  {
+    image: image6,
+    text: "პრ აქცია სკოლებში",
+  },
+  {
+    image: image7,
+    text: "გორის სკ-ს სპორტული აქცია",
+  },
+  {
+    image: image8,
+    text: "პროექტის ქართველი მონაწილეების ვიზიტი კანადაში",
+  },
+  {
+    image: image9,
+    text: "პატრიცია კაირსის ვიზიტი",
+  },
+  {
+    image: image10,
+    text: "Canadian Project Members visit to Georgia",
+  },
+  {
+    image: image11,
+    text: "Seminars held in CPDCS GTU for Gori, Cnori and Akhalkhalaki Univeristy teachers",
+  },
+  {
+    image: image12,
+    text: "Pictures taken after military actions in Gori",
+  },
+  {
+    image: image13,
+    text: "Meeting of Authors and teachers of II year Textbooks for Gori Community College",
+  },
+  {
+    image:image14,
+    text: "Awarding Teachers with Certificates in MES",
+  },
+  {
+    image: image15,
+    text: "სახელმძღვანელოების პრეზენტაცია",
+  },
+  {
+    image: image16,
+    text: "მარკუს გრანიუსის ვიზიტი",
+  },
+  {
+    image: image17,
+    text: "Seminars Provided by Steven Mickelson and Joe Monahan in Gori CC",
+  },
+  {
+    image: image18,
+    text: "Wade Miller seminar in Gori Community College for Teachers and Students",
+  },
+  {
+    image: image19,
+    text: "ს მაიკელსონის და ს. მონაჰანის ვიზიტი საქართველოში",
+  },
+  {
+    image: image20,
+    text: "Steven K Mickelsons visit in Gori University and meeting with rector Z. Cotniashvili",
+  },
+  {
+    image: image21,
+    text: "U.S. Visitors Trip to Gori. Meeting with Gori University Rector,",
+  },
+  {
+    image: image22,
+    text: "ვეიდ მილერის სემინარი თბილისში",
+  },
+  {
+    image: image23,
+    text: "The agreement between some bussines representatives and GTU",
+  },
+  {
+    image: image24,
+    text: "Category 342",
+  },
+  {
+    image: image25,
+    text: "გორის სათემო კოლეჯის ღია კარის დღე",
+  },
+  {
+    image: image26,
+    text: "Meeting With Ministry of Education and Science about New GPS devices Bought by Scientific Funding",
+  },
+  {
+    image: image27,
+    text: "Energetics Meeting In Gallery About Energy Sources",
+  },
+  {
+    image: image28,
+    text: "ლალი ბაქრაძის სემინარი",
+  },
+  {
+    image: image29,
+    text: "Trainings provided in CETL for Batumi Teachers",
+  },
+  {
+    image: image30,
+    text: "Trainings Provided in Cetl for Gori Teachers",
+  },
+  {
+    image: image31,
+    text: "აკადემიური შეხვედრა",
+  },
+  {
+    image: image32,
+    text: "Opening Community College In Gori.",
+  },
+  {
+    image: image33,
+    text: "First Course Subjects Textbooks Presentation, 2-9.07",
+  },
+  {
+    image: image34,
+    text: "Professional development - Program Presentation in Construction Management. 06.2007",
+  },
+  {
+    image: image35,
+    text: "Seminars for various university Teachers by Rhys Andrews 06.2007",
+  },
+  {
+    image: image36,
+    text: "Seminars for various university students by Rhys Andrews 06.2007",
+  },
+  {
+    image: image37,
+    text: "SENCER for Development TLC 23.02.2007.",
+  },
+  {
+    image: image38,
+    text: "Provision Stages of Program Accreditation 05.2007",
+  },
+  {
+    image: image39,
+    text: "MAry lou Lawer and Ed Stossels Trip and Seminars",
+  },
+  {
+    image: image40,
+    text: "teacher Training Provided by MES. Held in Gallery, Photo review",
+  },
+  {
+    image: image41,
+    text: "USAID ორგანიზებული შეხვედრა გალერეაში",
+  },
+  {
+    image: image42,
+    text: "USA Summer School 2005, Photo Review",
+  },
+  {
+    image: image43,
+    text: "TSU TLC 2004",
+  },
+];
