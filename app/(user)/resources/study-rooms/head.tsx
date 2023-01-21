72d945be400f201e502d12db743dd526e9bf18b4
lasha <lashatatu@gmail.com>
2023-01-21 20:03:12 +0400

    building banner component 2:36:09
    fixed aligment
    fixed menu bar focus
    
    Administration            done
    
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      done
    directions                done
    
    Workshops                 done
    
    trainings                 done
    seminars                  done
    us-summer school          done
    
    Projects                  done
    
    current-projects          done
    finished-projects         done
    
    Library
    
    new-textbooks             done
    supporting-materials      partially done needs review
    center-library            done
    gallery                   done
    memorandums               not done
    
    Resources
    
    employees                done
    study-rooms              not done
    trained-teachers         not done
    NATO-video-studio        not done

---
+++ app/(user)/resources/study-rooms/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>სასწავლო ოთახები</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/(user)/resources/trained-teachers/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>გადამზადებული მასწავლებლები</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
