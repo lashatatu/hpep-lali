d238ded86f76614b043d6682d402dc0295d94dd8
lasha <lashatatu@gmail.com>
2023-01-21 20:34:37 +0400

    building banner component 2:36:09
    fixed aligment
    fixed menu bar focus
    
    Administration            done
    
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      done
    directions                done
    
    Workshops                 done
    
    trainings                 done
    seminars                  done
    us-summer school          done
    
    Projects                  done
    
    current-projects          done
    finished-projects         done
    
    Library
    
    new-textbooks             done
    supporting-materials      partially done needs review
    center-library            done
    gallery                   done
    memorandums               not done
    
    Resources
    
    employees                done
    study-rooms              done
    trained-teachers         not done
    NATO-video-studio        done

---
+++ utilities/nato-images.ts
@@ -0,0 +1,31 @@
+import image1 from "../public/nato/nato1.jpg";
+import image2 from "../public/nato/nato2.jpg";
+import image3 from "../public/nato/nato3.jpg";
+import image4 from "../public/nato/nato4.jpg";
+import image5 from "../public/nato/nato5.jpg";
+import image6 from "../public/nato/nato6.png";
+import image7 from "../public/nato/nato7.jpg";
+
+export const natoImages = [
+  {
+    image: image1,
+  },
+  {
+    image: image2,
+  },
+  {
+    image: image3,
+  },
+  {
+    image: image4,
+  },
+  {
+    image: image5,
+  },
+  {
+    image: image6,
+  },
+  {
+    image: image7,
+  },
+];
