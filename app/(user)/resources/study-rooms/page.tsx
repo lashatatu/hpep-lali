f26348c62398f229dff1d8e8081912fdcf7ed2a5
lasha <lashatatu@gmail.com>
2023-01-21 20:23:57 +0400

    building banner component 2:36:09
    fixed aligment
    fixed menu bar focus
    
    Administration            done
    
    advisory-board            done
    advisory-committee        done
    consultants               done
    guests-of-the-center      done
    directions                done
    
    Workshops                 done
    
    trainings                 done
    seminars                  done
    us-summer school          done
    
    Projects                  done
    
    current-projects          done
    finished-projects         done
    
    Library
    
    new-textbooks             done
    supporting-materials      partially done needs review
    center-library            done
    gallery                   done
    memorandums               not done
    
    Resources
    
    employees                done
    study-rooms              done
    trained-teachers         not done
    NATO-video-studio        not done

--- app/(user)/resources/study-rooms/page.tsx
+++ app/(user)/resources/study-rooms/page.tsx
@@ -1,9 +1,27 @@
+import { classroomImages } from "../../../../utilities/classroom-images";
+import Image from "next/image";
 const Page = () => {
-  return (
-    <div>
-      <p>Page</p>
-    </div>
-  )
-}
 
-export default Page
+  return (
+    <div className={"lt-container-small"}>
+      <h1 className={"lt-page-h1"}>სასწავლო ოთახები</h1>
+      <div className={"grid grid-cols-2 gap-y-8 gap-x-2 mx-10"}>
+        {classroomImages.map((item, i) => (
+          <div className={'border-2'}>
+            <Image
+              // @ts-ignore
+              key={i}
+              src={item.image}
+              alt={"photo"}
+              className={'mx-auto object-bottom p-2'}
+              height={200}
+              width={300}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ utilities/classroom-images.ts
@@ -0,0 +1,59 @@
+import image1 from "../public/classroom-images/classroom-image-1.jpg";
+import image2 from "../public/classroom-images/classroom-image-2.jpg";
+import image3 from "../public/classroom-images/classroom-image-3.jpg";
+import image4 from "../public/classroom-images/classroom-image-4.jpg";
+import image5 from "../public/classroom-images/classroom-image-5.jpg";
+import image6 from "../public/classroom-images/classroom-image-6.jpg";
+import image7 from "../public/classroom-images/classroom-image-7.jpg";
+import image8 from "../public/classroom-images/classroom-image-8.jpg";
+import image9 from "../public/classroom-images/classroom-image-9.jpg";
+import image10 from "../public/classroom-images/classroom-image-10.jpg";
+import image11 from "../public/classroom-images/classroom-image-11.jpg";
+import image12 from "../public/classroom-images/classroom-image-12.jpg";
+import image13 from "../public/classroom-images/classroom-image-13.jpg";
+import image14 from "../public/classroom-images/classroom-image-14.jpg";
+
+export const classroomImages = [
+  {
+    image: image1,
+  },
+  {
+    image: image2,
+  },
+  {
+    image: image3,
+  },
+  {
+    image: image4,
+  },
+  {
+    image: image5,
+  },
+  {
+    image: image6,
+  },
+  {
+    image: image7,
+  },
+  {
+    image: image8,
+  },
+  {
+    image: image9,
+  },
+  {
+    image: image10,
+  },
+  {
+    image: image11,
+  },
+  {
+    image: image12,
+  },
+  {
+    image: image13,
+  },
+  {
+    image: image14,
+  },
+];
