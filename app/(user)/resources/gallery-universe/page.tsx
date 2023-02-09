4151d18371dcc312edb1203d846739de985d34d3
lasha <lashatatu@gmail.com>
2023-02-09 05:58:52 +0400

    finished needs review from lali

--- app/(user)/resources/gallery-universe/page.tsx
+++ app/(user)/resources/gallery-universe/page.tsx
@@ -1,4 +1,8 @@
-import {artists, colleagues, kids} from '../../../../utilities/gallery-universe'
+import {
+  artists,
+  colleagues,
+  kids,
+} from "../../../../utilities/gallery-universe";
 import Image from "next/image";
 
 const Page = () => {
@@ -15,6 +19,35 @@ const Page = () => {
               <div
                 className={
                   "flex flex-col justify-between overflow-hidden rounded-3xl border-2"
+                }
+              >
+                <p className={"p-2 text-center indent-0 text-primary"}>
+                  {kid.description}
+                </p>
+                <div>
+                  <Image
+                    key={index}
+                    // @ts-ignore
+                    src={kid.image}
+                    alt={kid.description}
+                    className={"mx-auto object-bottom"}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={"mx-10"}>
+        <div>
+          <h2 className={"lt-page-h2 pl-0 text-center"}>
+            ბავშვების ნამუშევრები
+          </h2>
+          <div className={"grid grid-cols-4 gap-y-8 gap-x-2 pb-16"}>
+            {kids.map((kid, index) => (
+              <div
+                className={
+                  "flex h-max flex-col overflow-hidden rounded-3xl border-2"
                 }
               >
                 <p className={"p-2 text-center indent-0 text-primary"}>
