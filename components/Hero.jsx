af014ccb7ec2394d8738721b0c1fe7fb3f5a1a0c
lasha <lashatatu@gmail.com>
2023-05-01 14:02:52 +0400

    added personal exhibitions

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,12 +7,10 @@ const Hero = () => {
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-white">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">აქ იქნება სლოგანი</h1>
+          <div className="max-w-96">
+            <h1 className="mb-5 text-5xl font-bold">საქართველოს ტექნიკური უნიბერსიტეტის პროფესიული განვითარების ცენტრი</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+
             </p>
             <button className="btn font-bold">დაწყება</button>
           </div>
