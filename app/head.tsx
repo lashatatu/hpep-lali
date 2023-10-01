d0aa4f6d9cb6b11bed0504a395336e60da601453
lasha <lashatatu@gmail.com>
2023-10-02 00:17:21 +0400

    added fivicon

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>HPEP.GE</title>
+      <title>{`სტუ-ს აკადემიური და მასწავლებელთა პერსონალის გადამზადების ცენტრი | HPEP.GE`}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
