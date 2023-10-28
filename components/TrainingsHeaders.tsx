5685870e15717881932e42d57324455455d12e91
lasha <lashatatu@gmail.com>
2023-10-28 23:45:14 +0400

    updated trainings

--- components/TrainingsHeaders.tsx
+++ components/TrainingsHeaders.tsx
@@ -1,14 +1,11 @@
 // @ts-ignore
-const TrainingsHeaders = ({h2Header,h3Date,faculty}) => {
+const TrainingsHeaders = ({h2Header, h3Date, faculty}) => {
   return (
     <div>
-      <h3 className={"lt-page-h3 text-xl"}>
-        თრენინგი “{h2Header}” განხორციელდა:
-      </h3>
-      <h3 className={"lt-page-h3 text-xl"}>
-        {h3Date}
-        <br/>
-        {faculty}ს შემდეგი აკადემიური პერსონალისთვის:
+      <h3 className={'lt-page-h3 text-xl'}>
+        <p className={'pb-2'}>თრენინგი “{h2Header}” განხორციელდა:</p>
+        <p className={'pb-2'}>{h3Date}</p>
+        <p>{faculty}ს შემდეგი აკადემიური პერსონალისთვის:</p>
       </h3>
     </div>
   )
