b8f127a9bbd7732a9ee15a86c662b5a012b74fa0
lasha <lashatatu@gmail.com>
2023-10-28 23:34:23 +0400

    updated trainings

--- components/TrainingsHeaders.tsx
+++ components/TrainingsHeaders.tsx
@@ -2,10 +2,10 @@
 const TrainingsHeaders = ({h2Header,h3Date,faculty}) => {
   return (
     <div>
-      <h2 className={"lt-page-h2"}>
+      <h3 className={"lt-page-h3 text-xl"}>
         თრენინგი “{h2Header}” განხორციელდა:
-      </h2>
-      <h3 className={"lt-page-h3"}>
+      </h3>
+      <h3 className={"lt-page-h3 text-xl"}>
         {h3Date}
         <br/>
         {faculty}ს შემდეგი აკადემიური პერსონალისთვის:
